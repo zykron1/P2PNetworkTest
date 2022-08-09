1404bb09fcacf5cf8525bedd10f3d6e58f8f1214
Ahsan Ahmed <ahsan@Ahsanserver.local>
2022-08-08 17:52:25 -0700

    Added the ablility to store data

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ print("Socket successfully created")
 #Define the port
 port = 2000
 
+#Setup QuickStore
+QuickStore = []
+
 #Bind the port to the socket
 s.bind(('', port))        
-print("socket binded to %s" %(port))
+print("Socket binded to %s" %(port))
 
 #Have the socket into Listen Mode
 s.listen(5)
@@ -20,14 +23,21 @@ s.listen(5)
 while True:
     # Establish connection with client.
     c, addr = s.accept()    
-    print ('Got connection from', addr )
-
-    # send a thank you message to the client. encoding to send byte type.
-    c.send('Thank you for connecting'.encode())
-    
+    print ('Got connection from', addr ) 
     #Print the clients message
     request = c.recv(1024).decode()
     print(f"[MESSAGE] {addr} sent {request}")
-
+    
+    #Parse the request
+    phrases = request.split(" ")
+    if phrases[0] == "MakeConn":
+        #Send a message back to the client
+        QuickStore.append(addr)
+        c.send(str(len(QuickStore)-1).encode())
+    if phrases[0] == "GetConn":
+        try:
+            c.send(str(QuickStore[int(phrases[1])][).encode())
+        except:
+            c.send("Invalid Input".encode())
     # Close the connection with the client
     c.close() 
